9be68728f29cc2054bc8fbbc28e5f22ee20df235
Elyorbek-2 <boyxonovelyorbek@gmail.com>
2021-11-18 21:22:32 +0500

    bir

---
+++ src/main/java/uz/pdp/tajriba/TajribaApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.tajriba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TajribaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TajribaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/tajriba/TajribaApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.tajriba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TajribaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
